50d72ea0e752260542280921603fefaa108140f9
Nuntapong <nuntapong@odds.team>
2022-11-10 23:27:15 +0700

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import express from "express";
 const main = () => {
   const app = express();
 
+  app.use((err, _, res, __) => {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  });
+
   app.get("/api/health", (_, res) => {
     res.status(200).json({ message: "YOU GOT IT" });
   });
